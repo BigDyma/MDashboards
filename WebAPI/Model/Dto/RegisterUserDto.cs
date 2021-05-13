afc5ab96b264c98c5b1f9c29b377b9bc90b148df
Dumitru Strelet <dumitru.strelet@amdaris.com>
2021-05-13 15:32:14 +0300

    Added BaseController & Started work on AuthController

---
+++ WebAPI/Model/Dto/RegisterUserDto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Model.Dto
+{
+    public class RegisterUserDto 
+    {
+        [MaxLength(32)]
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        [MaxLength(320)]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
